3cadadaae01556973dc5754b595977c2e9ca70f5
ADMIN <bgj443@myy.haaga-helia.fi>
2022-03-14 21:02:42 +0200

    valmis

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as Speech from "expo-speech";
+import React, { useState } from "react";
+import { Pressable, StyleSheet, Text, TextInput, View } from "react-native";
+
 
 export default function App() {
+  const [userInput, setUserInput] = useState("");
+
+  const speak = () => {
+    Speech.speak(userInput);
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TextInput style={styles.input} placeholder="Text to speak"
+        onChangeText={(userInput) => setUserInput(userInput)}
+        value={userInput}/>
+      <Pressable style={styles.pressable} onPress={speak}>
+        <Text style={styles.text}>Press to hear</Text>
+      </Pressable>
     </View>
   );
 }
@@ -13,8 +24,33 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  input:{
+    marginTop: 30,
+    fontSize: 20,
+    width: 300,
+    borderColor: "black",
+    borderWidth: 2,
+    margin: 15,
+  },
+  pressable: {
     alignItems: 'center',
     justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 25,
+    borderRadius: 6,
+    elevation: 6,
+    backgroundColor: 'darkblue',
+    marginBottom: 30,
+  },
+  text: {
+    fontSize: 15,
+    lineHeight: 20,
+    fontWeight: 'bold',
+    letterSpacing: 0.30,
+    color: 'white',
   },
 });
